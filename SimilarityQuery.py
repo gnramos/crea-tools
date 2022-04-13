63a41453e207ab4619519c51cb5a3399c6362166
wallacebt <wallacebtlw@gmail.com>
2022-04-13 15:13:31 -0300

    Added an option to do a thresholded query

--- SimilarityQuery.py
+++ SimilarityQuery.py
@@ -8,10 +8,16 @@ print()
 n = int(input("Type how many most similar documents will be shown:\n\t"))
 print()
 
+# TODO: It's probably better to insert the threshold as function parameters separate into 2 function calls: 1 to n-best and other for >= threshold
+t = float(input("Would you like to set up a minimum value? [0, 1], type -1 for no:\n\t"))
+print()
+
 result = search_similarity_query(text, n)
 
-if result.empty:
-    print("There were no similar subjects to your inserted text\n")
+if (t != -1):
+    result = result[result['Relevancia'] >= t]
 
+if result.empty:
+    print("There were no similar subjects to your inserted text or selected threshold\n")
 else:
     print(result)
