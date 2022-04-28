d71100ed551141185009aef32cbec3256fb16214
gnramos <ramos@gnramos.com>
2022-04-28 15:53:54 -0300

    Refatoração de SimilarityQuery.

--- src/creatools.py
+++ src/creatools.py
@@ -171,7 +171,7 @@ def preprocess(text:str):
     return output
 
 # SimilarityFunction.py ##########################################################
-def search_similarity_query(filename:str, search_document:str, num_best:int=8):
+def search_similarity_query(filename, search_document, num_best):
     """ Based on the the given text, it will perform a similarity search with the model already trained and return a Dataframe containing the best results.
 
     Based on the filename given that define the subjects, this function preprocesses the given text and computes the cossine similarity value for each class (subject) that is stored in the model. These values are then sorted in decreacresing order and stored in a Dataframe, which will finally be returned by the function.
@@ -229,31 +229,20 @@ def search_similarity_query(filename:str, search_document:str, num_best:int=8):
 
     return output
 
-# SimilarityQuery.py ##########################################################
-while(1):
-    filename = input("Type the name of the file that will provide the subjects (without extension)\n\t")
-    if os.path.isfile(filename + '.json'):
-        break
-    else:
-        print('Invalid file, check if the file exists and is typed correctly!\n')
-print()
 
-text = input("Type the text that will be used in the query :\n\t")
-print()
+def main():
+    course = input('Course name: ')
+    query = input('Query text: ')
+    n = input('Number of similar documents to show (default is 8): ')
+    n = int(n) if n else 8
+    threshold = input('Minimum similarity threshold value: ')
+    threshold = float(threshold) if threshold else 0
 
-n = int(input("Type how many most similar documents will be shown:\n\t"))
-print()
+    result = search_similarity_query(course, query, n)
+    result = result[result['Relevancia'] >= threshold]
 
-# TODO: Maybe it's better to use the threshold in a second function. Therefore, 2 functions will be created: one to n-best and other for >= threshold
-t = float(input("Would you like to set up a minimum value? [0, 1], type -1 if you don't:\n\t"))
-print()
-
-result = search_similarity_query(filename, text, n)
-
-if (t != -1):
-    result = result[result['Relevancia'] >= t]
-
-if result.empty:
-    print("There were no similar subjects to your inserted text or selected threshold\n")
-else:
     print(result)
+
+
+if __name__ == '__main__':
+    main()
