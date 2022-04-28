d9ee27d2c4f1a73219917773b649fb62f6aae38a
gnramos <ramos@gnramos.com>
2022-04-28 17:08:09 -0300

    Refatoração de SimilarityFunction.

--- src/creatools.py
+++ src/creatools.py
@@ -170,64 +170,49 @@ def preprocess(text:str):
 
     return output
 
-# SimilarityFunction.py ##########################################################
-def search_similarity_query(filename, search_document, num_best):
-    """ Based on the the given text, it will perform a similarity search with the model already trained and return a Dataframe containing the best results.
 
-    Based on the filename given that define the subjects, this function preprocesses the given text and computes the cossine similarity value for each class (subject) that is stored in the model. These values are then sorted in decreacresing order and stored in a Dataframe, which will finally be returned by the function.
+def search_similarity_query(course, query, num_best=8):
+    '''Apply the course model to search for query within subjects.
 
-    ### Parameters:
-        filename: a string type object containing the name of the subjects file (without the extension).
-        search_document: a string type object containing the text that will be used in the similarity query.
-        num_best: an integer type object which will limit the number of similar subjects returned. By default, this limit will be 8.
+    Keyword arguments:
+    course -- a string containing the name of course.
+    query -- a string containing the text to be searched for.
+    num_best -- the number of similar subjects shown.
 
-    ### Returns:
-        A Dataframe type object which will contain the results of the query. This can also be an empty Dataframe if no cossine similarity value is significant enough.
-    """
+    Returns:
+        A DataFrame with the query results.
+    '''
 
-    subjects_df = pd.read_json(filename + '.json')
-    subjects_df = subjects_df.sort_values(by=["codigo"])
+    subjects_df = pd.read_json(f'{course}.json')
+    subjects_df = subjects_df.sort_values(by=['codigo'])
     subjects_df = subjects_df.reset_index(drop=True)
-    try:
-        lsi_corpus = gensim.corpora.MmCorpus('AuxFiles\\lsi_model-' + filename + '_mm')
-        with open('AuxFiles\\dict_and_models_pickle-' + filename + '.txt', 'rb') as f:
-            id2wordDict, tfidf_model, lsi_model = pickle.load(f)
-    except FileNotFoundError:
-        modelTraining(filename=filename)
 
-        lsi_corpus = gensim.corpora.MmCorpus('AuxFiles\\lsi_model-' + filename + '_mm')
-        with open('AuxFiles\\dict_and_models_pickle-' + filename + '.txt', 'rb') as f:
-            id2wordDict, tfidf_model, lsi_model = pickle.load(f)
+    lsi_corpus_file = f'AuxFiles\\lsi_model-{course}_mm'
+    if not os.path.isfile(lsi_corpus_file):
+        modelTraining(course)
+    lsi_corpus = gensim.corpora.MmCorpus(lsi_corpus_file)
 
-    cosineSimilarity = MatrixSimilarity(lsi_corpus, num_features = lsi_corpus.num_terms, num_best=num_best)
+    file = f'AuxFiles\\dict_and_models_pickle-{course}.txt'
+    with open(file, 'rb') as f:
+        id2wordDict, tfidf_model, lsi_model = pickle.load(f)
 
-    # * preprocessing and processing until becomes a matrix of type term_to_topic (V)
-    doc = preprocess(search_document)
+    cosineSimilarity = MatrixSimilarity(lsi_corpus,
+                                        num_features=lsi_corpus.num_terms,
+                                        num_best=num_best)
+
+    doc = preprocess(query)
     query_bow = id2wordDict.doc2bow(doc)
     query_tfidf = tfidf_model[query_bow]
     query_lsi = lsi_model[query_tfidf]
 
-    # * cossine similarity between the vector of the new document vs all other vectors of documents
-    # * returns a list of top num_best tuples (id of compared document, similarity)
-    ranking = cosineSimilarity[query_lsi]
-
-    ranking.sort(key=lambda unit: unit[1], reverse= True)
-    result = []
-
-    for subject in ranking:
-
-        result.append (
-            {
-                'Relevancia': round((subject[1] * 100),6),
-                'Código da Matéria': subjects_df['codigo'][subject[0]],
-                'Nome da matéria': subjects_df['nome'][subject[0]]
-            }
-
-        )
-
-    output = pd.DataFrame(result, columns=['Relevancia','Código da Matéria','Nome da matéria'])
-
-    return output
+    ranking = sorted(cosineSimilarity[query_lsi],
+                     key=lambda unit: unit[1], reverse=True)
+    result = [{'Relevancia': round((subject[1] * 100), 6),
+               'Código da Matéria': subjects_df['codigo'][subject[0]],
+               'Nome da matéria': subjects_df['nome'][subject[0]]}
+              for subject in ranking]
+    return pd.DataFrame(result, columns=['Relevancia', 'Código da Matéria',
+                                         'Nome da matéria'])
 
 
 def main():
