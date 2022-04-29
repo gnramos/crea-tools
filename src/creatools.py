473b67b4077eb1ffc3492b4ec42b90e95672b391
gnramos <ramos@gnramos.com>
2022-04-29 11:33:12 -0300

    Refatoração para clareza de código.

--- src/creatools.py
+++ src/creatools.py
@@ -1,7 +1,7 @@
 from gensim.corpora import MmCorpus
 from gensim.similarities.docsim import MatrixSimilarity
-from nltk.corpus import stopwords
 import gensim
+import nltk.corpus
 import os
 import pandas as pd
 import pickle
@@ -18,29 +18,10 @@ def _get_course_from_json(json_file):
     return course
 
 
-def data_preprocessing(json_file):
-    '''Dumps the course information into a pickle file..
-
-    Reads the course's JSON file containing all subjects and builds the
-    DataFrame with joined name, syllabus and content.
-
-    Keyword arguments:
-    json_file -- path the the file with the course data.
-    '''
-
-    subjects_df = pd.read_json(json_file)
-    subjects_df = subjects_df.sort_values(by=["codigo"])
-    subjects_df = subjects_df.reset_index(drop=True)
-
-    # Provavelmente pode ser feito de modo mais eficiente diretamente no subjects_df
-    documents_list = [preprocess(f'{row["nome"]} {row["ementa"]} {row["conteudo"]}')
-                      for i, row in subjects_df.iterrows()]
-    documents_series = pd.Series(documents_list, name='documento')
-    documents_df = pd.concat([subjects_df, documents_series], axis=1)
-
-    course = _get_course_from_json(json_file)
-    with open(_tmp_dir(f'{course}_documents_df.pkl'), 'wb') as f:
-        pickle.dump(documents_df, f)
+def _get_subject_df(json_file):
+    df = pd.read_json(json_file)
+    df = df.sort_values(by=["codigo"])
+    return df.reset_index(drop=True)
 
 
 def model_training(json_file):
@@ -53,18 +34,28 @@ def model_training(json_file):
     json_file -- path the the file with the course data.
     '''
 
-    course = _get_course_from_json(json_file)
-    file = _tmp_dir(f'{course}_documents_df.pkl')
-    if not os.path.isfile(file):
-        data_preprocessing(json_file)
+    def document_df():
+        course = _get_course_from_json(json_file)
+        file = _tmp_dir(f'{course}_documents_df.pkl')
+        if os.path.isfile(file):
+            return pd.read_pickle(file)
 
-    with open(file, 'rb') as f:
-        documents_df = pickle.load(f)
+        subjects_df = _get_subject_df(json_file)
 
-    lemmatizedData = documents_df["documento"].tolist()
+        # Provavelmente pode ser feito de modo mais eficiente diretamente no subjects_df
+        documents_list = [preprocess(f'{row["nome"]} {row["ementa"]} {row["conteudo"]}')
+                          for i, row in subjects_df.iterrows()]
+        documents_series = pd.Series(documents_list, name='documento')
+        df = pd.concat([subjects_df, documents_series], axis=1)
+        df.to_pickle(file)
+        return df
+
+    df = document_df()
+    lemmatizedData = df["documento"].tolist()
     id2wordDict = gensim.corpora.Dictionary(lemmatizedData)
     corpus = [id2wordDict.doc2bow(text) for text in lemmatizedData]
 
+    course = _get_course_from_json(json_file)
     tfidf_model = gensim.models.TfidfModel(corpus, id2word=id2wordDict)
     MmCorpus.serialize(_tmp_dir(f'{course}_tfidf_mm'), tfidf_model[corpus])
 
@@ -80,16 +71,16 @@ def model_training(json_file):
 
 
 nlp = spacy.load('pt_core_news_lg')
-ignore = ["i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix", "x", "xi"]
-stop_words = stopwords.words("portuguese")
-stop_words += ['referente', 'seguinte', 'etc', 'ª', 'tal', 'um', 'dois', 'tres',
-               'vs', 'aula', 'tal']
-stop_words = gensim.utils.simple_preprocess(" ".join(stop_words), deacc=True,
-                                            min_len=1, max_len=40)  # magic numbers?
+ignore = ['i', 'ii', 'iii', 'iv', 'v', 'vi', 'vii', 'viii', 'ix', 'x', 'xi']
+stopwords = nltk.corpus.stopwords.words('portuguese')
+stopwords += ['referente', 'seguinte', 'etc', 'ª', 'tal', 'um', 'dois', 'tres',
+              'vs', 'aula', 'tal']
+stopwords = gensim.utils.simple_preprocess(' '.join(stopwords), deacc=True,
+                                           min_len=1, max_len=40)  # magic numbers?
 
-intervention_dict = {"campar": "campo",
-                     "seriar": "serie",
-                     "eletromagnetico": "eletromagnetismo"}
+intervention_dict = {'campar': 'campo',
+                     'seriar': 'serie',
+                     'eletromagnetico': 'eletromagnetismo'}
 
 
 def preprocess(text):
@@ -106,16 +97,17 @@ def preprocess(text):
     A list type object containing all words as lemmas.
     '''
 
-    text_lst = gensim.utils.simple_preprocess(text, deacc=True, min_len=1,
-                                              max_len=40)  # magic numbers?
-    text_str = " ".join([word for word in text_lst if word not in ignore])
-    text_doc = nlp(text_str)
-    lemmatized_text = [token.lemma_
-                       for token in text_doc
-                       if token.text not in stop_words]
+    def lemmatized_text():
+        text_lst = gensim.utils.simple_preprocess(text, deacc=True, min_len=1,
+                                                  max_len=40)  # magic numbers?
+        text_str = ' '.join([word for word in text_lst if word not in ignore])
+        text_doc = nlp(text_str)
+        return [token.lemma_
+                for token in text_doc
+                if token.text not in stopwords]
 
     return [intervention_dict.get(token, token)
-            for token in lemmatized_text
+            for token in lemmatized_text()
             if len(token) > 1]
 
 
@@ -132,33 +124,31 @@ def search_similarity_query(json_file, query, num_best=8, threshold=0):
         A list of tuples with the resulting disciplines in the format
         (relevance, code, name).
     '''
+    def get_ranking(course):
+        lsi_corpus_file = _tmp_dir(f'{course}_lsi_mm')
+        if not os.path.isfile(lsi_corpus_file):
+            model_training(json_file)
+        lsi_corpus = gensim.corpora.MmCorpus(lsi_corpus_file)
 
-    subjects_df = pd.read_json(json_file)
-    subjects_df = subjects_df.sort_values(by=['codigo'])
-    subjects_df = subjects_df.reset_index(drop=True)
+        with open(_tmp_dir(f'{course}_dict+models.pkl'), 'rb') as f:
+            id2wordDict, tfidf_model, lsi_model = pickle.load(f)
+
+        cosineSimilarity = MatrixSimilarity(lsi_corpus,
+                                            num_features=lsi_corpus.num_terms,
+                                            num_best=num_best)
+
+        query_bow = id2wordDict.doc2bow(preprocess(query))
+        query_tfidf = tfidf_model[query_bow]
+        query_lsi = lsi_model[query_tfidf]
+
+        return sorted(cosineSimilarity[query_lsi],
+                      key=lambda unit: unit[1], reverse=True)
 
     course = _get_course_from_json(json_file)
-    lsi_corpus_file = _tmp_dir(f'{course}_lsi_mm')
-    if not os.path.isfile(lsi_corpus_file):
-        model_training(json_file)
-    lsi_corpus = gensim.corpora.MmCorpus(lsi_corpus_file)
 
-    with open(_tmp_dir(f'{course}_dict+models.pkl'), 'rb') as f:
-        id2wordDict, tfidf_model, lsi_model = pickle.load(f)
-
-    cosineSimilarity = MatrixSimilarity(lsi_corpus,
-                                        num_features=lsi_corpus.num_terms,
-                                        num_best=num_best)
-
-    doc = preprocess(query)
-    query_bow = id2wordDict.doc2bow(doc)
-    query_tfidf = tfidf_model[query_bow]
-    query_lsi = lsi_model[query_tfidf]
-
-    ranking = sorted(cosineSimilarity[query_lsi],
-                     key=lambda unit: unit[1], reverse=True)
-    return [(relevance, subjects_df['codigo'][idx], subjects_df['nome'][idx])
-            for idx, relevance in ranking
+    df = _get_subject_df(json_file)
+    return [(relevance, df['codigo'][idx], df['nome'][idx])
+            for idx, relevance in get_ranking(course)
             if relevance >= threshold]
 
 
